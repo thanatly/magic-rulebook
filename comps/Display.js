8cce078b87e51684f7cc0047f0315a224bfe1901
thanatly <82842370+thanatly@users.noreply.github.com>
2021-08-10 17:15:25 +0300

    Add comments on functionality

--- comps/Display.js
+++ comps/Display.js
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 import styles from '../styles/Rules.module.css'
 
+// Display Section
 function Section ({rule}){
     return <h3 key={rule.id}> { rule.id }) {rule.text}</h3>
 }
 
+// Display Chapter with a dynamic hyperlink to rules
 function Chapter ({rule}){
     return  <Link href={'/rules/' + rule.id} key={rule.id} >
     <a className={styles.single}> 
@@ -14,6 +16,7 @@ function Chapter ({rule}){
     </Link>;
 }
 
+// Display rules
 function Rule ({rule}){
     return <h4 className={styles.text} key={rule.id}>{ rule.id } {rule.text}</h4>
 }
@@ -25,6 +28,13 @@ const Display = (props) => {
     if (newSearch){
         return <Rule rule={rule}/>
     } else {
+        /* Determine which class to be displayed of based on the lenght of rule id.
+        Not optimal because we need to know the exact format of rule id.
+        For example, 
+            "1. Game Concepts" is Section
+            "100. General" is Chapter
+            "100.1 These Magic rules..." is Rule
+        */
         const id_length = rule.id.length
         if(id_length === 1){
             return <Section rule={rule}/>
--- pages/api/util.js
+++ pages/api/util.js
@@ -1,7 +1,10 @@
 export default async function getRules() {
-    const handler = await fetch('https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'); // Fetch or do something else
+    const handler = await fetch('https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt');
     var response = await handler.text();
 
+    /* Extract only rule component from the whole text by specifying the string before and after.
+    Not optimal because it's context specific. To fetch rules from any url, allow user input the start and end string?
+    */
     response = response.split("Credits\r\n\r\n")[1]
     response = response.split("\r\n\r\nGlossary")[0]
     response = response.split("\r\n\r\n")
@@ -15,6 +18,23 @@ export default async function getRules() {
         }      
         rules.push(rule) 
     }
-    
+
+    /* Return as rules = [
+      {
+        id: 1,
+        text: 'Game Concepts'
+      },
+      {
+        id: 100,
+        text: 'General'
+      }, 
+      {
+        id: 100.1,
+        text: 'These Magic rules...'
+      }, 
+      ...
+    ]
+    */
+
     return rules;
   }
