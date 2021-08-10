4f56e06ae1dd0083fdacd7da54eaad59d7a5580e
thanatly <82842370+thanatly@users.noreply.github.com>
2021-08-10 16:47:47 +0300

    Update About and Add Google Analytics

--- pages/about.js
+++ pages/about.js
@@ -10,12 +10,12 @@ const About = () => {
       </Head>
       <div>
         <h1>About</h1>
-        <p>Natalie is a software developer based in Helsinki, Finland. Born and raised in Bangkok, Thailand, she completed a B.Eng in Control and System Engineering from Tokyo Institute of Technology in Japan. Currently, she is doing a master’s degree in Entrepreneurship Innovation Management at Aalto University. She is doing a minor in Computer Sciences (Web Technologies, Applications, and Science Track).</p>
-        <p>She is a business-oriented engineer who is passionate about tech startups. She excels on a collaborative project where creativity meets functionality. Her work motivation comes from new challenges outside her comfort zone. Her previous roles as a Chief Curator at Slush Tokyo and a Technical Account Manager at Goldman Sachs demonstrate strong product management and communication skills.</p>
-        <p>This website was built with React.js on the Next.js framework. It was auto-deployed and hosted on Vercel. The rule of the game was fetched from the text file here. Next.js was a convenient framework to create dynamic URL routing for each chapter.</p>
-        <a className={styles.btn} href="https://github.com/thanatly/magic-rulebook"> See Source Code</a>
-        <a className={styles.btn} href="https://github.com/thanatly"> Github</a>
-        <a className={styles.btn} href="https://www.linkedin.com/in/nutthanuntha/"> Linkedin</a>
+        <p>Natalie is a software developer based in Helsinki, Finland. She completed a bachelor’s degree in Control and System Engineering from Tokyo Institute of Technology. Currently, she is pursuing a master’s degree in Entrepreneurship Innovation Management at Aalto University. Her minor is Computer Science - Web Technologies, Applications, and Science Track. She is fluent in English, Japanese and Thai.</p>
+        <p>She is a business-oriented engineer. Her work motivation comes from new challenges outside her comfort zone. She excels on a collaborative project where creativity meets functionality. Her previous roles as a Chief Curator at Slush Tokyo and a Technical Account Manager at Goldman Sachs demonstrate strong product management and communication skills.</p>
+        <p>This website was built with React.js on the Next.js framework, allowing a convenient dynamic URL routing. It was auto-deployed and hosted on Vercel. The rule of the game was fetched from the text file <a href="https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt"> here</a>. </p>
+        <a className={styles.btn} href="https://github.com/thanatly/magic-rulebook">See Source Code</a>
+        <a className={styles.btn} href="https://github.com/thanatly">Github</a>
+        <a className={styles.btn} href="https://www.linkedin.com/in/nutthanuntha/">Linkedin</a>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,29 @@ export default function Home() {
       <Head>
         <title>Magic the Gathering | Home</title>
         <meta name="keywords" content="rules"/>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-C15VS54KZ6`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C15VS54KZ6', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
       <div>
         <h1 className={styles.title}>Magic the Gathering</h1>
         <p className={styles.text}>Welcome! This web application contains the rule book of Wizards of the Coast. The content is divided into 9 sections. Each section contains multiple chapters. When clicking the chapter, the page will redirect to a new page which contains a list of all rules in that chapter. You can search for the rule including a particular word by typing in the search box (case sensitive). </p>
         <p className={styles.text}>This page is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.</p>
         <Link href="/rules/">
-          <a className={styles.btn}>See content</a>
+          <a className={styles.btn}>See Rules</a>
         </Link>
       </div>
     </>
